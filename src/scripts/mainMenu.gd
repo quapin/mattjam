6f52de2335fdc71f6b11b25fd40b88aed4ecb7b5
Emmakasaki <153797674+Emmakasaki@users.noreply.github.com>
2023-12-26 15:26:25 -0700

    Main Menu Edits

--- src/scripts/mainMenu.gd
+++ src/scripts/mainMenu.gd
@@ -2,15 +2,13 @@ extends Control
 
 
 
-
-
+# When Start Button gets released
 func _on_start_button_button_up():
 	get_tree().change_scene_to_file("res://levels/test level.tscn")
-
+	
+# When Quit Button gets released
 
 
 
 func _on_quit_button_button_up():
 	get_tree().quit()
-
-
