10290c3cae5f56e997c06bc47d7f6d1ca780c624
Emmakasaki <153797674+Emmakasaki@users.noreply.github.com>
2023-12-26 20:05:36 -0700

    More Main Menu Updates

---
+++ src/scripts/UI/mainMenu.gd
@@ -0,0 +1,56 @@
+extends Control
+
+var labelStart = LabelSettings.new()
+var labelQuit = LabelSettings.new()
+var font = load("res://assets/buttons/alagard.ttf")
+
+func _ready():
+	labelStart.set_font_color(Color.WHITE)
+	labelStart.set_font(font)
+	labelStart.set_font_size(130)
+	labelStart.set_shadow_size(15)
+	labelStart.set_shadow_color(Color.BLACK)
+	labelStart.set_shadow_offset(Vector2(-4, 1))
+	$StartButton/StartLabel.label_settings = labelStart
+	
+	labelQuit.set_font_color(Color.WHITE)
+	labelQuit.set_font(font)
+	labelQuit.set_font_size(130)
+	labelQuit.set_shadow_size(15)
+	labelQuit.set_shadow_color(Color.BLACK)
+	labelQuit.set_shadow_offset(Vector2(-4, 1))
+	$QuitButton/QuitLabel.label_settings = labelQuit
+	
+# When Start Button gets released
+func _on_start_button_button_up():
+	get_tree().change_scene_to_file("res://levels/test level.tscn")
+	
+# When Start Label is hovered over
+func _on_start_label_mouse_entered():
+	labelStart.set_font_color(Color.html("FF9A9A"))
+	labelStart.set_font_size(140)
+	labelStart.set_shadow_size(16)
+	labelStart.set_shadow_offset(Vector2(-7, 1))
+func _on_start_label_mouse_exited():
+	labelStart.set_font_color(Color.WHITE)
+	labelStart.set_font_size(130)
+	labelStart.set_shadow_size(15)
+	labelStart.set_shadow_offset(Vector2(-4, 1))
+
+# When Quit Button gets released
+func _on_quit_button_button_up():
+	get_tree().quit()
+	
+# When Quit Label is hovered over
+func _on_quit_label_mouse_entered():
+	labelQuit.set_font_color(Color.html("FF9A9A"))
+	labelQuit.set_font_size(140)
+	labelQuit.set_shadow_size(16)
+	labelQuit.set_shadow_offset(Vector2(-7, 1))
+func _on_quit_label_mouse_exited():
+	labelQuit.set_font_color(Color.WHITE)
+	labelQuit.set_font_size(130)
+	labelQuit.set_shadow_size(15)
+	labelQuit.set_shadow_offset(Vector2(-4, 1))
+	
+
